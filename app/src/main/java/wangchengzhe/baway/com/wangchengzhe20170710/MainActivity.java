b6cb76e73a74a30e1462758ccd4bf3b12709de6e
470301559 <470301559@qq.com>
2017-07-10 09:01:06 +0800

    aa

---
+++ app/src/main/java/wangchengzhe/baway/com/wangchengzhe20170710/MainActivity.java
@@ -0,0 +1,13 @@
+package wangchengzhe.baway.com.wangchengzhe20170710;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
